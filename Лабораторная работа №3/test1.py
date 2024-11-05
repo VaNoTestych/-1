5e203f25615a71959ed32ab830cdb3a52e005834
VaNoTestych <iptestov05@gmail.com>
2024-11-05 20:23:20 +0300

    Update test1.py

--- Лабораторная работа №3/test1.py
+++ Лабораторная работа №3/test1.py
@@ -1,8 +1,8 @@
 # TODO Напишите функцию для поиска индекса товара
 
-def search(items_list, item):
-    for i, search_item in enumerate(items_list):
-        if search_item == item:
+def search(items_list, item): #Создал функцию search для поиска в списках
+    for i, search_item in enumerate(items_list): #За счет цикла for и enumerate получаем индекс и сам предмет, а потом сравниваем с тем, который передали в функцию
+        if search_item == item: 
             return i
 
     return None
@@ -10,7 +10,7 @@ def search(items_list, item):
 items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
 
 for find_item in ['банан', 'груша', 'персик']:
-    index_item = search(items_list, find_item)
+    index_item = search(items_list, find_item) #Применяем функцию на наши списки
     if index_item is not None:
         print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
     else:
